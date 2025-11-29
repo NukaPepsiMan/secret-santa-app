8abc1950879de6c18a8ae3efea019942ac03a36f
NukaPepsiMan <davidelicone@hotmail.it>
2025-11-29 23:38:22 +0100

    feat(HeroUI): aggiunta HeroUI

--- secret-santa-app/tailwind.config.js
+++ secret-santa-app/tailwind.config.js
@@ -1,5 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+const {heroui} = require("@heroui/react");
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -8,6 +9,7 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.jsx',
+        "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
     ],
 
     theme: {
@@ -18,5 +20,7 @@ export default {
         },
     },
 
-    plugins: [forms],
+    darkMode: "class",
+
+    plugins: [forms, heroui()],
 };
