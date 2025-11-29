7c2e14dbeb9a7ab598dc802419c05c838c94aa4e
NukaPepsiMan <davidelicone@hotmail.it>
2025-11-30 00:57:55 +0100

    feat(events): aggiunte properties e relazione con l'utenza ed organizzatore

--- secret-santa-app/app/Models/Event.php
+++ secret-santa-app/app/Models/Event.php
@@ -9,4 +9,20 @@ class Event extends Model
 {
     /** @use HasFactory<\Database\Factories\EventFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'organizer_id',
+        'name',
+        'exchange_date',
+        'drawn_at',
+    ];
+
+    protected $casts = [
+        'exchange_date' => 'date',
+        'drawn_at' => 'datetime'
+    ];
+
+    public function organizer() {
+        return $this->belongsTo(User::class, 'organizer_id');
+    }
 }
