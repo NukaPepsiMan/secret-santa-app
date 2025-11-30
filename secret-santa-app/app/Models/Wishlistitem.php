9c055f61f20db271d1514db05b41dec2b68622c0
NukaPepsiMan <davidelicone@hotmail.it>
2025-11-30 23:54:05 +0100

    feat(wishlist): generazione modello wishlist +entity

---
+++ secret-santa-app/app/Models/Wishlistitem.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Wishlistitem extends Model
+{
+    /** @use HasFactory<\Database\Factories\WishlistitemFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'participant_id',
+        'name',
+    ];
+
+    public function participant() {
+        return $this -> belongsTo(Participant::class);
+    }
+}
